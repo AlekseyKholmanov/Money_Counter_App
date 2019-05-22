5dd3e7a54d40fedc5cc664bd06dd4adb2f20e263
a.holmanov <a.holmanov@gmail.com>
2019-05-22 20:16:35 +0500

    add botoom navigation bar

--- app/src/androidTest/java/com/example/holmi_production/money_counter_app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/holmi_production/money_counter_app/ExampleInstrumentedTest.kt
@@ -1,8 +1,8 @@
 package com.example.holmi_production.money_counter_app
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
 
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
 
--- app/src/main/java/com/example/holmi_production/money_counter_app/MainActivity.kt
+++ app/src/main/java/com/example/holmi_production/money_counter_app/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.holmi_production.money_counter_app
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_main -> {
+                }
+                R.id.action_costs -> {
+                }
+                R.id.action_chart -> {
+                }
+            }
+            true
+        }
     }
 }
