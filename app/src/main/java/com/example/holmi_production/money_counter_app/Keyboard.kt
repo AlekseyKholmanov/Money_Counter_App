bb753032cef1fdca35807625d0f0016737b678c3
a.holmanov <a.holmanov@gmail.com>
2019-05-25 01:49:26 +0500

    listeners for button

--- app/src/main/java/com/example/holmi_production/money_counter_app/Keyboard.kt
+++ app/src/main/java/com/example/holmi_production/money_counter_app/Keyboard.kt
@@ -19,15 +19,30 @@ class Keyboard @JvmOverloads constructor(
     init {
         Log.d("qwerty", "init")
         View.inflate(context, R.layout.numbers_keyboard, this)
-        key_0.setOnClickListener { mKeyboardListener.numberPressed(0) }
-        key_000.setOnClickListener { mKeyboardListener.numberPressed(100) }
+        key_0.setOnClickListener { mKeyboardListener.numberPressed("0") }
+        key_000.setOnClickListener { mKeyboardListener.numberPressed("000") }
+        key_1.setOnClickListener { mKeyboardListener.numberPressed("1") }
+        key_2.setOnClickListener { mKeyboardListener.numberPressed("2") }
+        key_3.setOnClickListener { mKeyboardListener.numberPressed("3") }
+        key_4.setOnClickListener { mKeyboardListener.numberPressed("4") }
+        key_5.setOnClickListener { mKeyboardListener.numberPressed("5") }
+        key_6.setOnClickListener { mKeyboardListener.numberPressed("6") }
+        key_7.setOnClickListener { mKeyboardListener.numberPressed("7") }
+        key_8.setOnClickListener { mKeyboardListener.numberPressed("8") }
+        key_9.setOnClickListener { mKeyboardListener.numberPressed("9") }
+        key_divider.setOnClickListener { mKeyboardListener.numberPressed(".") }
+        key_delete.setOnClickListener { mKeyboardListener.deletePressed() }
+        key_enter.setOnClickListener { mKeyboardListener.enterPressed() }
     }
 
-    fun setListener(mKeyboardListener: IKeyboardListener){
+    fun setListener(mKeyboardListener: IKeyboardListener) {
         this.mKeyboardListener = mKeyboardListener
     }
 
 }
+
 interface IKeyboardListener {
-    fun numberPressed(number:Int)
+    fun numberPressed(number: String)
+    fun enterPressed()
+    fun deletePressed()
 }
--- app/src/main/java/com/example/holmi_production/money_counter_app/MainFragment.kt
+++ app/src/main/java/com/example/holmi_production/money_counter_app/MainFragment.kt
@@ -1,17 +1,52 @@
 package com.example.holmi_production.money_counter_app
 
-import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.holmi_production.money_counter_app.mvp.AndroidXMvpAppCompatFragment
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : AndroidXMvpAppCompatFragment(), IKeyboardListener {
-    override fun numberPressed(number: Int) {
-        expense.text = number.toString()
+
+    override fun enterPressed() {
+        Snackbar.make(main_fragment, text, Snackbar.LENGTH_SHORT).show()
+        text = "0"
+        displaySum()
     }
+
+    override fun deletePressed() {
+        text = text.dropLast(1)
+        if (text.takeLast(1) == ".")
+            text = text.dropLast(1)
+        displaySum()
+    }
+
+    override fun numberPressed(simbol: String) {
+        if(simbol=="."&&text.contains("."))
+            return
+        val pattern ="\\w.\\d+".toRegex()
+        val found = pattern.findAll(text)
+        found.forEach { v->
+
+            Log.d("qwerty1",v.value)
+        }
+        text += simbol
+        displaySum()
+    }
+
+    private fun displaySum() {
+        if (text.contains('.')) {
+            val float = text.toFloat()
+            val str = String.format("%.1f", float)
+            expense.text = str
+        } else
+            expense.text = text
+    }
+
+    private var text: String = ""
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.main_fragment, container, false)
