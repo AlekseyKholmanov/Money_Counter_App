be359ddb0450d63514112b748edba828df219460
a.holmanov <a.holmanov@gmail.com>
2019-06-01 13:13:03 +0500

    show date picker on date field

--- app/src/main/java/com/example/holmi_production/money_counter_app/firstLaunch/FirstActivity.kt
+++ app/src/main/java/com/example/holmi_production/money_counter_app/firstLaunch/FirstActivity.kt
@@ -1,5 +1,6 @@
 package com.example.holmi_production.money_counter_app.firstLaunch
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -9,6 +10,8 @@ import com.example.holmi_production.money_counter_app.App
 import com.example.holmi_production.money_counter_app.R
 import com.example.holmi_production.money_counter_app.mvp.AndroidXMvpAppCompatActivity
 import kotlinx.android.synthetic.main.first_launch_activity.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FirstActivity : AndroidXMvpAppCompatActivity(), FirstLaunchView, TextWatcher {
     override fun afterTextChanged(s: Editable?) {
@@ -24,6 +27,21 @@ class FirstActivity : AndroidXMvpAppCompatActivity(), FirstLaunchView, TextWatch
 
     override fun displayDateActivity() {
 
+    }
+    var c: Calendar = Calendar.getInstance()
+    var dateDialog: DatePickerDialog.OnDateSetListener =
+        DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+            c.set(Calendar.YEAR,year)
+            c.set(Calendar.MONTH,monthOfYear)
+            c.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+            updateLabel()
+        }
+
+    private fun updateLabel() {
+        val myFormat = "dd/MM/yy" //In which you need put here
+        val sdf = SimpleDateFormat(myFormat, Locale.UK)
+
+        date.setText(sdf.format(c.time))
     }
 
     @InjectPresenter
@@ -42,6 +60,17 @@ class FirstActivity : AndroidXMvpAppCompatActivity(), FirstLaunchView, TextWatch
         }
         start_sum.addTextChangedListener(this)
         date.addTextChangedListener(this)
+        date.setOnClickListener {
+            val picker = DatePickerDialog(
+                this,
+                dateDialog,
+                c.get(Calendar.YEAR),
+                c.get(Calendar.MONTH),
+                c.get(Calendar.DAY_OF_MONTH)
+            )
+            picker.datePicker.minDate = System.currentTimeMillis()-1000
+            picker.show()
+        }
     }
 
 }
