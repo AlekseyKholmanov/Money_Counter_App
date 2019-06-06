344478b15dbebc9ae72e3c3e7579978c783cb398
a.holmanov <a.holmanov@gmail.com>
2019-06-07 02:17:21 +0500

    keyboard like constraint + divide logic to keyboard

--- app/src/main/java/com/example/holmi_production/money_counter_app/main/Keyboard.kt
+++ app/src/main/java/com/example/holmi_production/money_counter_app/main/Keyboard.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.RelativeLayout
+import android.widget.TextView
 import com.example.holmi_production.money_counter_app.R
 import com.example.holmi_production.money_counter_app.model.ButtonTypes
+import com.example.holmi_production.money_counter_app.toCurencyFormat
 import kotlinx.android.synthetic.main.keyboard.view.*
 
 class Keyboard @JvmOverloads constructor(
@@ -16,26 +19,65 @@ class Keyboard @JvmOverloads constructor(
     defStyleRes: Int = 0
 ) : RelativeLayout(context, attrs, defStyle, defStyleRes) {
 
+
     private lateinit var mKeyboardListener: IKeyboardListener
+    var sum =""
 
     init {
         Log.d("qwerty", "init")
         View.inflate(context, R.layout.keyboard, this)
-        key_0.setOnClickListener { mKeyboardListener.buttonPressed( ButtonTypes.ZERO,"0") }
-        key_000.setOnClickListener { mKeyboardListener.buttonPressed(ButtonTypes.ZERO,"000") }
-        key_1.setOnClickListener { mKeyboardListener.buttonPressed(ButtonTypes.NUMERIC,"1") }
-        key_2.setOnClickListener { mKeyboardListener.buttonPressed(ButtonTypes.NUMERIC,"2") }
-        key_3.setOnClickListener { mKeyboardListener.buttonPressed(ButtonTypes.NUMERIC,"3") }
-        key_4.setOnClickListener { mKeyboardListener.buttonPressed(ButtonTypes.NUMERIC,"4") }
-        key_5.setOnClickListener { mKeyboardListener.buttonPressed(ButtonTypes.NUMERIC,"5") }
-        key_6.setOnClickListener { mKeyboardListener.buttonPressed(ButtonTypes.NUMERIC,"6") }
-        key_7.setOnClickListener { mKeyboardListener.buttonPressed(ButtonTypes.NUMERIC,"7") }
-        key_8.setOnClickListener { mKeyboardListener.buttonPressed(ButtonTypes.NUMERIC,"8") }
-        key_9.setOnClickListener { mKeyboardListener.buttonPressed(ButtonTypes.NUMERIC,"9") }
-        key_divider.setOnClickListener { mKeyboardListener.buttonPressed(ButtonTypes.DIVIDER,".") }
-        key_delete.setOnClickListener { mKeyboardListener.buttonPressed(ButtonTypes.DELETE) }
-        key_enter.setOnClickListener { mKeyboardListener.buttonPressed(ButtonTypes.ENTER) }
+
+        key_0.setOnClickListener { pressed(ButtonTypes.ZERO,"0") }
+        key_1.setOnClickListener {pressed(ButtonTypes.NUMERIC,"1") }
+        key_2.setOnClickListener {pressed(ButtonTypes.NUMERIC,"2") }
+        key_3.setOnClickListener { pressed(ButtonTypes.NUMERIC,"3") }
+        key_4.setOnClickListener { pressed(ButtonTypes.NUMERIC,"4") }
+        key_5.setOnClickListener { pressed(ButtonTypes.NUMERIC,"5") }
+        key_6.setOnClickListener { pressed(ButtonTypes.NUMERIC,"6") }
+        key_7.setOnClickListener { pressed(ButtonTypes.NUMERIC,"7") }
+        key_8.setOnClickListener { pressed(ButtonTypes.NUMERIC,"8") }
+        key_9.setOnClickListener {pressed(ButtonTypes.NUMERIC,"9") }
+        key_divider.setOnClickListener { pressed(ButtonTypes.DIVIDER,".") }
+        key_delete.setOnClickListener { pressed(ButtonTypes.DELETE) }
+        key_enter.setOnClickListener { pressed(ButtonTypes.ENTER) }
     }
+
+    fun pressed(type:ButtonTypes, value: String? = null){
+        when (type) {
+            ButtonTypes.DELETE -> {
+                sum = sum.dropLast(1)
+                if (sum.takeLast(1) == ".")
+                    sum = sum.dropLast(1)
+            }
+            ButtonTypes.DIVIDER -> {
+                when {
+                    value == "." && sum.contains(".") -> return
+                    sum == "" -> sum = "0."
+                    else -> sum += value
+                }
+            }
+            //TODO triple zero bug when 0.
+            ButtonTypes.ZERO -> {
+                when (sum) {
+                    "" -> return
+                    else -> sum += value
+                }
+            }
+            ButtonTypes.ENTER -> {
+                sum = ""
+            }
+            ButtonTypes.NUMERIC -> {
+                if (sum == "0")
+                    sum = ""
+                if (sum.contains('.') && sum.takeLast(1) != ".")
+                    sum = sum.dropLast(1)
+                sum += value
+            }
+        }
+        expense.text = sum
+    }
+
+
 
     fun setListener(mKeyboardListener: IKeyboardListener) {
         this.mKeyboardListener = mKeyboardListener
