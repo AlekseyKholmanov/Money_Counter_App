e4473a901e490b9853a60521aa887b36e9309c29
a.holmanov <a.holmanov@gmail.com>
2019-07-10 10:36:00 +0500

    send notification from receiver

--- app/src/main/java/com/example/holmi_production/money_counter_app/notification/NotificationAlarmReciever.kt
+++ app/src/main/java/com/example/holmi_production/money_counter_app/notification/NotificationAlarmReciever.kt
@@ -6,12 +6,15 @@ import android.content.Intent
 import android.util.Log
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.holmi_production.money_counter_app.App
+import com.example.holmi_production.money_counter_app.interactor.NotificationInteractor
 import com.example.holmi_production.money_counter_app.storage.SettingRepository
 import javax.inject.Inject
 
 class NotificationAlarmReciever: BroadcastReceiver() {
 
     @Inject lateinit var settingRepository: SettingRepository
+    @Inject lateinit var notificationInteractor: NotificationInteractor
+    //@Inject lateinit var notificationManager: NotificationManager
 
     override fun onReceive(context: Context, intent: Intent) {
 
@@ -20,8 +23,12 @@ class NotificationAlarmReciever: BroadcastReceiver() {
         val endDate = settingRepository.getTillEnd()
         if (endDate <= 0)
             settingRepository.setIsEnd(true)
-        send(context)
+        //send(context)
+
+        //alarmTriggred возвращает Disposabable. Во фрагментах presenters наследуются от BasePresenter куда собираются все подписки, здесь не очень понимаю как это можно сделать
+        notificationInteractor.alarmTriggered()
     }
+
 
     private fun send(context: Context) {
         val intent = Intent("custom-intent-filter")
