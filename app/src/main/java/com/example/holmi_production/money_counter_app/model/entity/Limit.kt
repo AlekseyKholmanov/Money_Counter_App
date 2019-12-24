544e6817a19f732e993fe42f58b3d678563a65e8
AlekseyKholmanov <a.holmanov@gmail.com>
2019-12-24 23:37:45 +0300

    limit model

---
+++ app/src/main/java/com/example/holmi_production/money_counter_app/model/entity/Limit.kt
@@ -0,0 +1,16 @@
+package com.example.holmi_production.money_counter_app.model.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.joda.time.DateTime
+
+@Entity
+data class Limit(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val categoryId:Int,
+    val month: Int,
+    val amount: Double,
+    val startDate:DateTime,
+    val endDate:DateTime)
